c61d77a2ec54fd55e61999512c747432b52af812
Marjo Forcado <marjoforcado@gmail.com>
2022-01-25 23:58:49 +0800

    Add translation icon

--- ui/components/Navbar/Navbar.tsx
+++ ui/components/Navbar/Navbar.tsx
@@ -24,10 +24,26 @@ const Navbar = () => {
           <div className={styles["content"]}>
             <h1 className={styles["brand"]}>Mrjfrcdo</h1>
             <div className={styles["items-right"]}>
+              <button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className={styles["icons"]}
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M3 5h12M9 3v2m1.048 9.5A18.022 18.022 0 016.412 9m6.088 9h7M11 21l5-10 5 10M12.751 5C11.783 10.77 8.07 15.61 3 18.129"
+                  />
+                </svg>
+              </button>
               <button onClick={() => setIsDropdownToggled((prev) => !prev)}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className={styles["toggle"]}
+                  className={styles["icons"]}
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
