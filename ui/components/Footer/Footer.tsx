ea489550e8226c5c554e9fd3d9d6ebcad52cea4a
Marjo Forcado <marjoforcado@gmail.com>
2022-01-27 14:55:33 +0800

    Add footer contents

--- ui/components/Footer/Footer.tsx
+++ ui/components/Footer/Footer.tsx
@@ -5,15 +5,28 @@ import styles from "./styles.module.scss";
 const Footer = () => (
   <footer className={styles["footer"]}>
     <Container centerHorizontal>
-      <p>Site Map</p>
-      <ul>
-        <li>About</li>
-        <li>Services</li>
-        <li>Portfolio</li>
-        <li>Blog</li>
-        <li>Contact</li>
-      </ul>
+      <div>
+        <p className={styles["title"]}>Site Map</p>
+        <ul className={styles["links"]}>
+          <li>About</li>
+          <li>Services</li>
+          <li>Portfolio</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div>
+        <p className={styles["title"]}>Other Links</p>
+        <ul className={styles["links"]}>
+          <li>Site Update</li>
+        </ul>
+      </div>
     </Container>
+    <div className={styles["sub-footer"]}>
+      <Container>
+        <p>Last Update Jan 2020</p>
+      </Container>
+    </div>
   </footer>
 );
 
